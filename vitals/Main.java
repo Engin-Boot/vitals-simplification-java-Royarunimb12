1fe0e72485d54d6f4ff438e192da3ae60e2d0771
Royarunimb12 <69623012+Royarunimb12@users.noreply.github.com>
2020-09-06 20:04:27 +0530

    Update Main.java

--- vitals/Main.java
+++ vitals/Main.java
@@ -6,19 +6,19 @@ public class Main {
     private static float[] Spo2_Limits={30,100};
     private static float[] RespRate_Limits={30,95};
 
-    static boolean isbpOk(float bpm){
+    public static boolean isbpOk(float bpm){
         return isVital_LimitsOk(bpm, BP_limits[0], BP_limits[1]);
     }
-    static boolean isSpo2Ok(float spo2)
+    public static boolean isSpo2Ok(float spo2)
     {
         return isVital_LimitsOk(spo2, Spo2_Limits[0], Spo2_Limits[1]);
     }
-    static boolean isRespRateOk(float respRate)
+    public static boolean isRespRateOk(float respRate)
     {
         return isVital_LimitsOk(respRate, RespRate_Limits[0], RespRate_Limits[1]);
     }
 
-    static boolean isVital_LimitsOk(float val, float high, float low)
+    public static boolean isVital_LimitsOk(float val, float high, float low)
     {
         return (val >= low && val <= high);
     }
