0df3376ea258e2d55ce6b8a88febf733e2d8cb1a
Royarunimb12 <69623012+Royarunimb12@users.noreply.github.com>
2020-09-07 11:40:32 +0530

    Update VitalsConditions.java

--- vitals/VitalsConditions.java
+++ vitals/VitalsConditions.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class VitalsConditions {
     public static List<Vitals> lvit = new ArrayList<>();
-    public static IReporter irep=ReporterFactory.getReporterInstance();
+    public static IReporter irep=new ReporterPlugins();
 
     static void addVital(Vitals vital) {
         lvit.add(vital);
