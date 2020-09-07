e7e721da95ad141e21d424795e7e9f4ed0122ded
Royarunimb12 <69623012+Royarunimb12@users.noreply.github.com>
2020-09-07 11:42:21 +0530

    Update Tests.java

--- vitals/Tests.java
+++ vitals/Tests.java
@@ -5,29 +5,29 @@ public class Tests {
         VitalsConditions.addVital(new Vitals("bpm",50,30,100));
         VitalsConditions.addVital(new Vitals("spo2",50,20,100));
         VitalsConditions.addVital(new Vitals("respRate",60,30,100));
-        if(VitalsConditions.checkIfAllVitalsOk(VitalsConditions.getallVitals())==true)
-            System.out.println("ok");
+        assert(VitalsConditions.checkIfAllVitalsOk(VitalsConditions.getallVitals())==true);
+           
         VitalsConditions.lvit.clear();
 
         VitalsConditions.addVital(new Vitals("bpm",102,30,100));
         VitalsConditions.addVital(new Vitals("spo2",50,20,100));
         VitalsConditions.addVital(new Vitals("respRate",60,30,100));
-        if(VitalsConditions.checkIfAllVitalsOk(VitalsConditions.getallVitals())==false)
-            System.out.println("ok");
+        assert(VitalsConditions.checkIfAllVitalsOk(VitalsConditions.getallVitals())==false);
+           
         VitalsConditions.lvit.clear();
 
         VitalsConditions.addVital(new Vitals("bpm",50,30,100));
         VitalsConditions.addVital(new Vitals("spo2",19,20,100));
         VitalsConditions.addVital(new Vitals("respRate",60,30,100));
-        if(VitalsConditions.checkIfAllVitalsOk(VitalsConditions.getallVitals())==false)
-            System.out.println("ok");
+        assert(VitalsConditions.checkIfAllVitalsOk(VitalsConditions.getallVitals())==false);
+         
         VitalsConditions.lvit.clear();
 
         VitalsConditions.addVital(new Vitals("bpm",50,30,100));
         VitalsConditions.addVital(new Vitals("spo2",50,20,100));
         VitalsConditions.addVital(new Vitals("respRate",150,30,100));
-        if(VitalsConditions.checkIfAllVitalsOk(VitalsConditions.getallVitals())==false)
-            System.out.println("ok");
+        assert(VitalsConditions.checkIfAllVitalsOk(VitalsConditions.getallVitals())==false);
+            
         VitalsConditions.lvit.clear();
 
 
